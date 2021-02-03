ed381e7d8b6aa4ec89292b0833168dbf33a4267b
David Tran <davtran303@gmail.com>
2021-02-02 18:34:27 -0700

    made search bar

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-//global variables
-var location = 0;
+function citySearch(){
+  var city ="london"
 
-//document.id to display information on card of each city
-document.getElementById("card-1").textContent
-document.getElementById("card-2").textContent
-document.getElementById("card-3").textContent
-document.getElementById("card-4").textContent
-document.getElementById("card-5").textContent
+  var apiKey = "378576e029da0c8c2b9ad196519798cf"
+
+  var queryUrlToday = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
+  //weather data today
+  $.ajax({
+      url: queryUrlToday,
+      method: "GET",
+  }).then(function(res) {
+      console.log(res)
+    document.getElementById("current-city").innerHTML= "Current City: " + res.name
+
+    document.getElementById("current-temp").textContent=Math.floor(res.main.temp) +  " °F"
+  }) 
+}
+
+function weatherCast(){
+  var apiKey = "378576e029da0c8c2b9ad196519798cf"
+
+  var queryUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey
+
+  //weather data forecast
+  $.ajax({
+      url: queryUrlForecast,
+      method: "GET",
+  }).then(function(res) {
+      console.log(res.list[1].main.temp)
+      document.getElementById("card-text-1").innerHTML = 5+6;
+      // console.log(res.list[1].main.temp)
+      // console.log(res.list[10].main.temp)
+      // console.log(res.list[20].main.temp)
+      // console.log(res.list[30].main.temp)
+      // console.log(res.list[40].main.temp)
+
+
+      // document.getElementById("card-text-1").textContent= "Temp : " + res.list[10].main.temp
+      // // console.log(res.city)
+  })
+}
 
 
 
-
-
-
-    //search button click on
-    document.getElementById("searchBtn").addEventListener("click", function(city){
-
-    var apiKey = "378576e029da0c8c2b9ad196519798cf"
-    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + city + apiKey
-
-
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
-    }).then(function(reponse) {
-        console.log(reponse)
-    })
-    
-    //temperature
-    var tempKel //temp kelvin
-    var tempFar = ((tempKel - 273.15) *1.8 + 32).toFixed(); //temp far formula
-
-    $(".city").text("City: " + reponse.name);
-    $(".wind").text("The current wind speend in " + city + "is at " + reponse.wind.speed);
-    $(".temp").text("The temperature is at " + tempFar + "Fahrenheit");
-    $(".humanity").text("The current humaidity is " + reponse.main.humidity);
-
-    saveCity();
-    });
+  
 
 //save button
-function saveCity() {
-  // Use JSON.parse() to convert text to JavaScript object
-  var location = JSON.parse(localStorage.getItem("city"));
-  // Check if data is returned, if not exit out of the function
-  if (saveCity !== null) {
-    event.preventDefault;
-    location++;
-    localStorage.setItem("city", location);
-    localStorage.setItem()
+// function saveCity() {
+// // Use JSON.parse() to convert text to JavaScript object
+// var location = JSON.parse(localStorage.getItem("city"));
+// // Check if data is returned, if not exit out of the function
+// if (saveCity !== null) {
+//   event.preventDefault;
+//   location++;
+//   localStorage.setItem("city", location);
+//   localStorage.setItem()
 
-  } else {
-    return;
-  }
-};
+// } else {
+//   return;
+// }
+// };
 
-saveButton.addEventListener("click", function(event){
-event.preventDefault();
-saveCity();
-
-});
+// var saveButton = document.getElementById("saveBtn")
+// saveButton.addEventListener("click", function(event){
+// event.preventDefault();
+// saveCity();
+// })
