db7b6f338034babca5bc46530009497a851a6ba4
David Tran <davtran303@gmail.com>
2021-02-10 20:32:08 -0700

    new files

--- script.js
+++ script.js
@@ -1,75 +1,38 @@
-function citySearch(){
-  var city ="london"
-
-  var apiKey = "378576e029da0c8c2b9ad196519798cf"
-
-  var queryUrlToday = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
-  //weather data today
-  $.ajax({
+function searchCity(){
+var city = document.getElementById("city").value
+var apiKey = "378576e029da0c8c2b9ad196519798cf"
+var queryUrlToday = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
+$.ajax({
       url: queryUrlToday,
       method: "GET",
-  }).then(function(res) {
-      console.log(res)
-    document.getElementById("current-city").innerHTML= "Current City: " + res.name
-
-    document.getElementById("current-temp").textContent=Math.floor(res.main.temp) +  " °F"
-
-   
-  }) 
+  }).then(function(res) { 
+     document.getElementById("current").textContent= "Current City : " + res.name
+     document.getElementById("temp").textContent = "Current Temp : " + Math.floor(res.main.temp) +  " °F"
+     document.getElementById("wind").textContent = "Current Wind Speed : " + res.wind.speed + " mph"
+     document.getElementById("humidity").textContent = "The Humidity : " + res.main.humidity + "%"
+     document.getElementById("weather").textContent = "Current Weather : " + res.weather[0].description
+  });
+  forecast();
 }
+ 
+function forecast(){
+var city = "London"
+var apiKeyFore = "378576e029da0c8c2b9ad196519798cf"
+var queryUrl ="https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" +apiKeyFore
 
-document.getElementById("myBtn").addEventListener("click", function() {
-  document.getElementById("demo").innerHTML = "Hello World";
-});
+$.ajax({
+    url: queryUrl,
+    method: "GET",
+}).then(function(res){
+    console.log(res)
+    for(var i = 0 ; i < 5 ; i++){
+        document.getElementById("day-" + (i +  1).toString() ).textContent = "Date/Time : " + res.list[i * 8].dt_txt
+        document.getElementById("weather-" + (i +  1).toString()).src = "http://openweathermap.org/img/w/" + res.list[i * 8].weather[0].icon + ".png"
+        document.getElementById("temp-" + (i +  1).toString()).textContent = "Temp : " + Math.floor(((res.list[i * 8].main.temp - 273.15) * 9/5 + 32)).toString() + " °F"
+        document.getElementById("hum-" + (i +  1).toString()).textContent = "Humidity : " +  res.list[i * 8].main.humidity
+    }
 
-
-
-function weatherCast(){
-  var apiKey = "378576e029da0c8c2b9ad196519798cf"
-
-  var queryUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey
-
-  //weather data forecast
-  $.ajax({
-      url: queryUrlForecast,
-      method: "GET",
-  }).then(function(res) {
-      console.log(res.list[1].main.temp)
-      document.getElementById("card-text-1").innerHTML = 5+6;
-      // console.log(res.list[1].main.temp)
-      // console.log(res.list[10].main.temp)
-      // console.log(res.list[20].main.temp)
-      // console.log(res.list[30].main.temp)
-      // console.log(res.list[40].main.temp)
+})
 
 
-      // document.getElementById("card-text-1").textContent= "Temp : " + res.list[10].main.temp
-      // // console.log(res.city)
-  })
 }
-
-
-
-  
-
-//save button
-// function saveCity() {
-// // Use JSON.parse() to convert text to JavaScript object
-// var location = JSON.parse(localStorage.getItem("city"));
-// // Check if data is returned, if not exit out of the function
-// if (saveCity !== null) {
-//   event.preventDefault;
-//   location++;
-//   localStorage.setItem("city", location);
-//   localStorage.setItem()
-
-// } else {
-//   return;
-// }
-// };
-
-// var saveButton = document.getElementById("saveBtn")
-// saveButton.addEventListener("click", function(event){
-// event.preventDefault();
-// saveCity();
-// })
